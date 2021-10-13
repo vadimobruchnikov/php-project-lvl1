308d03b102ffbf6c2defabdb77287e518f93ebca
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2021-10-13 03:10:30 +0300

    Step 4.2

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 install:
 	composer install
+
 brain-games:
 	php ./bin/brain-games
+
 validate:
 	composer validate
+
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
+
+lint-fix:
+	composer exec --verbose phpcbf -- --standard=PSR12 src bin
--- src/Cli.php
+++ src/Cli.php
@@ -1,9 +1,13 @@
-<?
+<?php
+
 namespace Brain\Games\Cli;
 
 use function cli\line;
 use function cli\prompt;
 
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+function greeting()
+{
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
