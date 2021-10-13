b3f73f9a5dd8b7a7c688dae1f90475f24d41c06c
Vadim Obruchnikov <vadimobruchnikov@yandex.ru>
2021-10-13 05:10:32 +0300

    Step 6

---
+++ src/Brain-calc.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Brain\Games\BrainCalc;
+
+use function cli\line;
+use function cli\prompt;
+
+line('Welcome to the Brain Game!');
+$username = prompt('May I have your name?');
+line("Hello, %s!", $username);
+line('What is the result of the expression?');
+$attemped = 1;
+$result = true;
+$operations = [ "+", "-", "*"];
+
+while ($attemped < 4 && $result) {
+    $number1 = random_int(1, 10);
+    $number2 = random_int(1, 10);
+    $operation = random_int(0, 2);
+    $expression = (string)$number1 . $operations[$operation] . $number2;
+    line("Question: $expression");
+    $answer = prompt('Your answer: ');
+    eval('$correctAnswer = ' . $expression . ';');
+    $result = $answer == $correctAnswer;
+    if (!$result) {
+        line("'$answer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
+        line("Let's try again, $username!");
+        return;
+    }
+    $attemped++;
+}
+line("Correct!");
+line("Congratulations, $username!");
